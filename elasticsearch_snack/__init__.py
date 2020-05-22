18519f058798da6475e39433f1070b1c0ad01fb6
Borja González Seoane <borja.gseoane@udc.es>
2020-05-22 13:35:39 +0200

    Start a Python package to closure the project code

---
+++ elasticsearch_snack/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+###########################################################
+# Elasticsearch snack 0.1
+#
+# A brief example case indexing recipes with Elasticsearch,
+# Python and Docker containers
+#
+# Copyright 2020 Borja González Seoane
+#
+# Contact: borja.gseoane@udc.es
+###########################################################
+
+__author__ = 'Borja González Seoane'
+__copyright__ = 'Copyright 2020, Borja González Seoane'
+__credits__ = 'Borja González Seoane'
+__license__ = 'LICENSE'
+__version__ = '0.0dev0'
+__maintainer__ = 'Borja González Seoane'
+__email__ = 'borja.gseoane@udc.es'
+__status__ = 'Experiment'
