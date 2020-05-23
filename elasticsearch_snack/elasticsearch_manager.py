d84b73e8292097100100d06144e9639d4f86756d
Borja González Seoane <borja.gseoane@udc.es>
2020-05-23 14:11:03 +0200

    Write a function to create an Elasticsearch index

--- elasticsearch_snack/elasticsearch_manager.py
+++ elasticsearch_snack/elasticsearch_manager.py
@@ -32,3 +32,55 @@ def connect_elasticsearch():
     else:
         raise ConnectionError('Elasticsearch unreachable')
     return es
+
+
+def create_snack_recipes_index(es_object):
+    """This function creates an Elasticsearch index to store Allrecipes recipes
+
+    This function creates an index to store the scrapped Allrecipes snack
+    recipes into Elasticsearch.
+
+    Warning: If the index already exists, this function overwrites it.
+
+    :param es_object: the Elasticsearch object instance
+    :raise Exception: for any error during the creation process
+    """
+
+    index_name = 'recipes'
+
+    # Index settings
+    settings = {
+        'settings': {
+            'number_of_shards': 1,
+            'number_of_replicas': 0
+        },
+        'mappings': {
+            'members': {
+                'dynamic': 'strict',
+                'properties': {
+                    'title': {
+                        'type': 'text'
+                    },
+                    'description': {
+                        'type': 'text'
+                    },
+                    'ingredients': {
+                        'type': 'text'
+                    },
+                    'nutrition': {
+                        'type': 'text'
+                    },
+                }
+            }
+        }
+    }
+
+    try:
+        if not es_object.indices.exists(index_name):
+            # Ignore 400 to ignore "Index already exist" error
+            es_object.indices.create(index=index_name,
+                                     ignore=400,
+                                     body=settings)
+            print('[OK]: Index created')
+    except Exception:
+        raise Exception('Error during the index creation process')
