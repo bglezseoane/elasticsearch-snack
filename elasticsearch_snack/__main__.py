aa30de9437eff9ee0ac5318e31fe29caa08ac751
Borja González Seoane <borja.gseoane@udc.es>
2020-05-23 18:33:21 +0200

    Collapse a full test case into the main script
    This version scrap the web information, index it into Elasticsearch and
    performs a search for a "Bruschetta" recipe.

--- elasticsearch_snack/__main__.py
+++ elasticsearch_snack/__main__.py
@@ -13,7 +13,24 @@
 
 """Main of Elasticsearch snack"""
 
+import json
+
+from elasticsearch_snack.elasticsearch_manager import connect_elasticsearch, \
+    create_snack_recipes_index, index_snack_recipes, search
 from elasticsearch_snack.scrapper import scrap_allrecipes_snack_recipes
 
 if __name__ == '__main__':
+    # Firstly scrap snack recipes from Allrecipes
     scrap_allrecipes_snack_recipes()
+
+    # Start the Elasticsearch engine
+    es = connect_elasticsearch()
+
+    # Create an index for the recipes and add our scrapped ones
+    create_snack_recipes_index(es)
+    index_snack_recipes(es)
+
+    # Prepare a query to search a "Bruschetta" recipe and print the results
+    search_object = {'query': {'match': {'title': 'Bruschetta'}}}
+    results = search(es, json.dumps(search_object))
+    print(results)
