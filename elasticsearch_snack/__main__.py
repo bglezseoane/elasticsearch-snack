7756090a85f601c5f11f13cb796ca039045d9e6b
Borja González Seoane <borja.gseoane@udc.es>
2020-05-25 12:38:16 +0200

    Delegate to the TUI module the core execution

--- elasticsearch_snack/__main__.py
+++ elasticsearch_snack/__main__.py
@@ -13,27 +13,7 @@
 
 """Main of Elasticsearch Snack"""
 
-import json
-
-from elasticsearch_snack.elasticsearch_manager import connect_elasticsearch, \
-    create_snack_recipes_index, index_snack_recipes, search
-from elasticsearch_snack.scrapper import scrap_allrecipes_snack_recipes
+from elasticsearch_snack.tui import start_tui
 
 if __name__ == '__main__':
-    # Firstly scrap snack recipes from Allrecipes
-    scrap_allrecipes_snack_recipes()
-
-    # Start the Elasticsearch engine
-    es = connect_elasticsearch()
-
-    # Create an index for the recipes and add our scrapped ones
-    create_snack_recipes_index(es)
-    index_snack_recipes(es)
-
-    # Prepare a query to search a "Bruschetta" recipe and print the results
-    search_object = {'query': {'match': {'title': 'Bruschetta'}}}
-    results = search(es, json.dumps(search_object))
-    results = results['hits']['hits']  # Access results ignoring metadata
-
-    for result in results:
-        print(result['_source']['title'])
+    start_tui()
